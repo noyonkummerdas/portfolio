0728c47c4b2f6b0a0cf730b74b1e9c2f8bdc5457
Nk Noyon <noyonkummerdas@gmail.com>
2026-02-05 23:51:38 +0600

    remove backround grainy-grandients

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -8,7 +8,7 @@ const Background = () => {
             <div className="absolute top-0 right-1/4 w-96 h-96 bg-yellow-500 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-2000"></div>
             <div className="absolute -bottom-8 left-1/3 w-96 h-96 bg-pink-500 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-4000"></div>
 
-            {/* <div className="absolute inset-0 bg-[url('https://grainy-gradients.vercel.app/noise.svg')] opacity-20 brightness-100 contrast-150"></div> */}
+            <div className="absolute inset-0 bg-[url('https://grainy-gradients.vercel.app/noise.svg')] opacity-20 brightness-100 contrast-150"></div>
         </div>
     );
 };
